326f85754d741bb2d1b7e74d67ceb3b5a9a78a39
Dmitry <dmitrytsepaev@gmail.com>
2019-01-25 01:53:55 +0300

    Some changes

--- image_parser.py
+++ image_parser.py
@@ -1,9 +1,11 @@
 import re, os, time
 from urllib.parse import unquote
 from urllib.request import urlretrieve
+from multiprocessing.dummy import Pool
 
 # assuming page.html exists in the current folder
 # try to read it with the most common html encodings
+
 for encoding in ('utf-8', 'windows-1251', 'iso-8859-1'):
 	try:
 		input_ = open('page.html', encoding = encoding)
@@ -22,14 +24,17 @@ print('Found %d images in %.3f seconds' % (len(urls), time.perf_counter() - star
 with open('urls.txt', 'w') as output:
 	output.write('\n'.join(urls))
 
+def download_url(url):
+	global target
+	path = target + '/' + unquote(url).split('/')[-1]
+	urlretrieve(url, path)
+
 if input('Download images? [Yes/No]\n').lower() == 'yes':
 	target = input('Choose folder: ').replace(' ', r'\ ')
 	if re.search(r'\w+?', target):
 		if not os.path.exists(target): os.mkdir(target)
-		count = 0
-		for url in urls:
-			count += 1
-			print('Downloading %d/%d' % (count, len(urls)))
-			path = target + '/' + unquote(url).split('/')[-1]
-			urlretrieve(url, path)
+		print('Downloading...')
+		start = time.perf_counter()
+		Pool(6).map(download_url, urls)
+		print('Done [%.3f seconds]' % (time.perf_counter() - start))
 	else: print('Incorrect folder name')
