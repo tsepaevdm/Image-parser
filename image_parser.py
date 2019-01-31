108746a3bed8d8814ea62e10666167986a28e2c7
Dmitry <dmitrytsepaev@gmail.com>
2019-02-01 00:28:43 +0300

    Some changes

--- image_parser.py
+++ image_parser.py
@@ -3,9 +3,6 @@ from urllib.parse import unquote
 from urllib.request import urlretrieve
 from multiprocessing.dummy import Pool
 
-# assuming page.html exists in the current folder
-# try to read it with the most common html encodings
-
 for encoding in ('utf-8', 'windows-1251', 'iso-8859-1'):
 	try:
 		input_ = open('page.html', encoding = encoding)
@@ -14,33 +11,29 @@ for encoding in ('utf-8', 'windows-1251', 'iso-8859-1'):
 	except: continue
 	finally: input_.close()
 
-# urls must contain only safe characters and '%' in case of the percent-encoding
-
 pattern = r"https?://[\w~?#!\[\]@'%/&()$.*:+,=;-]+?\.(?:gif|bmp|png|jpe?g)"
 start = time.perf_counter()
 urls = set(re.findall(pattern, raw_text))
 print('Found %d images in %.3f seconds' % (len(urls), time.perf_counter() - start))
-
-with open('urls.txt', 'w') as output:
-	output.write('\n'.join(urls))
+with open('urls.txt', 'w') as output: output.write('\n'.join(urls))
 
 def download_url(url):
-	global target, count
+	global target, count, width
 	try: 
 		count += 1
-		print('\n[%d/%d]' % (count, len(urls)), end = '')
+		print('\n[%*d/%d]' % (width, count, len(urls)), end = '')
 		path = target + '/' + unquote(url).split('/')[-1]
 		urlretrieve(url, path)
 	except: print(' Error!', end = '')
 
-
 if input('Download images? [Yes/No]\n').lower() == 'yes':
-	target = input('Choose folder: ').replace(' ', r'\ ')
+	target = input('Choose folder: ')
 	if re.search(r'\w+?', target):
 		if not os.path.exists(target): os.mkdir(target)
-		start = time.perf_counter()
 		count = 0
+		width = len(str(len(urls)))
 		print('Downloading...', end = '')
+		start = time.perf_counter()
 		Pool(6).map(download_url, urls)
-		print('Done [%.3f seconds]' % (time.perf_counter() - start))
+		print('\nDone [%.3f seconds]' % (time.perf_counter() - start))
 	else: print('Incorrect folder name')
